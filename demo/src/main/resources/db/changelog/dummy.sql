dc4920ef21806f9ea4a3d5f992a74f2ee4f24c8f
Mario <mario.martinsantana@gmail.com>
2023-01-27 09:37:32 +0100

    Dockerized:
    # .\mvnw package
    # java -jar target/demo-0.0.1-SNAPSHOT.jar
    
    # docker build --tag=demo:latest .
    # docker run -p8080:8080 demo:latest

--- demo/src/main/resources/db/changelog/dummy.sql
+++ demo/src/main/resources/db/changelog/dummy.sql
@@ -9,6 +9,12 @@ insert into person values (nextval('HIBERNATE_SEQUENCE'),'Mario');
 insert into account values (nextval('HIBERNATE_SEQUENCE'), 1);
 insert into operation values (nextval('HIBERNATE_SEQUENCE'), 2, 2,  {ts '2012-09-17 18:47:52.69'}, 'DEPOSIT', 2);
 
+# http://localhost:8080/h2-console/login.do
+
+# http://localhost:8080/api/person/all
+# http://localhost:8080/api/account/find/1
 # http://localhost:8080/api/account/withdrawal?account=1&amount=2
 
+
 # curl -d "accountId=1&amount=2" -X POST http://localhost:8080/api/account/withdrawal
+
