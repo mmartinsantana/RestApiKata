c320df47025b727e666bbbb9fdcf0f70900a6803
Mario <mario.martinsantana@gmail.com>
2023-02-16 15:22:28 +0100

    Implemented OAauth2 following https://spring.io/guides/tutorials/spring-boot-oauth2/
    TODO
    - Test and adapt the previous bank REST interface
    - Use JWT (JSON Web Token)

--- demo/src/main/java/com/example/kata/api_rest/demo/security/WebSecurityConfig.java
+++ demo/src/main/java/com/example/kata/api_rest/demo/security/WebSecurityConfig.java
@@ -1,64 +1,101 @@
 package com.example.kata.api_rest.demo.security;
 
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
+
 /**
  * https://www.baeldung.com/spring-security-login
  */
-//@Configuration
-//@EnableWebSecurity
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 
-	//@Bean
-	/*public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http
-				// h2 console: 	http://localhost:8080/h2-console/login.do
-				.headers(headers -> headers.frameOptions().disable())
+    @Bean
+    public WebClient rest(ClientRegistrationRepository clients, OAuth2AuthorizedClientRepository authz) {
+        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 =
+                new ServletOAuth2AuthorizedClientExchangeFilterFunction(clients, authz);
+        return WebClient.builder()
+                .filter(oauth2).build();
+    }
 
-				.csrf()
-				.disable()
+    @Bean
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2UserService(WebClient rest) {
+        DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+        return request -> {
+            OAuth2User user = delegate.loadUser(request);
+            if (!"github".equals(request.getClientRegistration().getRegistrationId())) {
+                return user;
+            }
 
-				.authorizeRequests()
-				.antMatchers("/api/**")
-				.hasRole("REST")
+            OAuth2AuthorizedClient client = new OAuth2AuthorizedClient
+                    (request.getClientRegistration(), user.getName(), request.getAccessToken());
+            String url = user.getAttribute("organizations_url");
+            List<Map<String, Object>> orgs = rest
+                    .get().uri(url)
+                    .attributes(oauth2AuthorizedClient(client))
+                    .retrieve()
+                    .bodyToMono(List.class)
+                    .block();
 
-				.antMatchers("/h2-console/**", "/login*")
-				.permitAll()
+            if (orgs.stream().anyMatch(org -> "spring-projects".equals(org.get("login")))) {
+                return user;
+            }
 
-				.anyRequest()
-				.authenticated()
+            throw new OAuth2AuthenticationException(new OAuth2Error("invalid_token", "Not in Spring Team", ""));
+        };
+    }
 
-				.and()
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        SimpleUrlAuthenticationFailureHandler handler = new SimpleUrlAuthenticationFailureHandler("/");
 
-				.formLogin()
-				//.loginPage("login.html")
-				.loginProcessingUrl("/perform_login")
-				.defaultSuccessUrl("/homepage.html", true)
-				.failureUrl("/login.html")
-				//.failureHandler(authenticationFailureHandler())
-
-				.and()
-
-				.logout()
-				.logoutUrl("/perform_logout")
-				.deleteCookies("JSESSIONID")
-				//.logoutSuccessHandler(logoutSuccessHandler());
-
-				.and()
-				.httpBasic();
-
-		;
-		return http.build();
-		// ...
-
-	}*/
-
-	//@Autowired
-	/*public void configureGlobal(AuthenticationManagerBuilder auth, DataSource dataSource, PasswordEncoder passwordEncoder) throws Exception {
-		auth.jdbcAuthentication().dataSource(dataSource)
-				.usersByUsernameQuery("select user_name as username, pass as password, true"
-						+ " from app_user where user_name=?")
-				.authoritiesByUsernameQuery("select user_name as username, authority"
-	}*/
-
+        // @formatter:off
+        http
+                .authorizeRequests(a -> a
+                        .antMatchers("/", "/error", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .logout(l -> l
+                        .logoutSuccessUrl("/").permitAll()
+                )
+                .csrf(c -> c
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                )
+                .oauth2Login(o -> o
+                        .failureHandler((request, response, exception) -> {
+                            request.getSession().setAttribute("error.message", exception.getMessage());
+                            handler.onAuthenticationFailure(request, response, exception);
+                        })
+                );
+        // @formatter:on
+        return http.build();
+    }
 
 }
---
+++ demo/src/main/java/com/example/kata/api_rest/demo/service/OAuth2UserServiceDemo.java
@@ -0,0 +1,7 @@
+package com.example.kata.api_rest.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OAuth2UserServiceDemo {
+}
